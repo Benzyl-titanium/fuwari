75fd4fc06d0e510d7b1d824fee13e67544d87ec2
Benzyl-titanium <benzyl.titanium@gmail.com>
2025-07-05 02:20:08 +0800

    update

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,6 @@
 import { siteConfig } from "@/config";
 import rss from "@astrojs/rss";
-import { getSortedPosts } from "@utils/content-utils";
+import { getSortedPostsByUpdated } from "@utils/content-utils";
 import type { APIContext } from "astro";
 import MarkdownIt from "markdown-it";
 import sanitizeHtml from "sanitize-html";
@@ -8,13 +8,13 @@ import sanitizeHtml from "sanitize-html";
 const parser = new MarkdownIt();
 
 export async function GET(context: APIContext) {
-	const blog = await getSortedPosts();
+	const posts = await getSortedPostsByUpdated();
 
 	return rss({
 		title: siteConfig.title,
 		description: siteConfig.subtitle || "No description",
 		site: context.site ?? "https://fuwari.vercel.app",
-		items: blog.map((post) => {
+		items: posts.map((post) => {
 			const content =
 				typeof post.body === "string" ? post.body : String(post.body || "");
 
